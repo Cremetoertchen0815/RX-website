a4e4993b4bc4a691361be61d11cea819f967f567
Cremetoertchen0815 <Mircosoft_heck@web.de>
2024-02-14 08:50:00 +0100

    Adapted animation to mobile devices

---
+++ js/container.js
@@ -0,0 +1,24 @@
+let contentLoaded = false;
+let animationComplete = false;
+
+startFadeOut = () => {
+    let cover = document.getElementById("pageCover");
+    cover.classList.add("fadeOut");
+    cover.onanimationend = () => console.log("peen");
+    cover.style.pointerEvents = "none";
+};
+
+const container = document.getElementById("pageContainer");
+container.src = "start.html";
+container.onload = () => {
+    contentLoaded = true;
+    if (animationComplete) startFadeOut();
+};
+
+
+
+const animated = document.getElementById("logoText");
+animated.addEventListener('animationend', () => {
+    animationComplete = true;
+    if (contentLoaded) startFadeOut();
+});
