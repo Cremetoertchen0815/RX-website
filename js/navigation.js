46d4ded28a76b926991728f9d1325b8a0bbc1017
Cremetoertchen0815 <Mircosoft_heck@web.de>
2024-02-14 15:00:09 +0100

    Continued improving navigation

--- js/navigation.js
+++ js/navigation.js
@@ -1,7 +1,13 @@
 //Narrow navigation
 let narrowNav = document.getElementById('navNarrow');
-let narrowNavList = document.getElementById('navNarrowList');
-narrowNav.onclick = () => narrowNavList.style.height = narrowNavList.style.height != "0%" ? "0%" : "100%";
+let navArrow = document.getElementById('navArrow');
+let narrowNavList = document.getElementById('navNarrowList'); // transform: rotate(180deg);
+narrowNav.onclick = () => {
+
+    let direction = narrowNavList.style.height != "0px";
+    narrowNavList.style.height = direction ? "0px" : "220px";
+    navArrow.style.transform = direction ? "rotate(0deg)" : "rotate(180deg)";
+}
 
 //Common navigation
 let browser = document.getElementById("pageContainer");
@@ -11,7 +17,8 @@ allNav.forEach(x => x.onclick = () =>
         let nuPath = x.getAttribute("destination");
         allNav.forEach(y => y.style.textDecoration = "none");
         x.style.textDecoration = "underline";
-        narrowNavList.style.height = "0%";
+        narrowNavList.style.height = "0px";
+        navArrow.style.transform = "rotate(0deg)";
         browser.setAttribute("src", nuPath);
     }
 );
