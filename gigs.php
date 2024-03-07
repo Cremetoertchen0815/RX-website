0094ee3d99ad7e3a286be33601a8baeca2f94e63
Cremetoertchen0815 <Mircosoft_heck@web.de>
2024-03-07 16:44:50 +0100

    Worked on designing a gig calender

--- gigs.php
+++ gigs.php
@@ -1,15 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/gigs.css">
     <title>Document</title>
 </head>
+
 <body>
     <h1>Gigs</h1>
 
-    <br><br><br><br><br>
-    <b style="text-align: center; display: inline-block; width: 100%;">Coming soon!</b>
+    <br><br>
+    <div class="gig">
+        <div class="gigLeft">
+            <h2>Sportlerfasching Spfr. Haßmersheim</h2>
+            <b>Eintritt: </b> 10€<br>
+            <span class="place">Turn- und Festhalle, Teststraße 7, 74855 Haßmersheim</span>
+        </div>
+        <div class="gigRight">
+            <span class="dateSpan">01.02.2024</span><br>
+            <b>Einlass:</b> 20 Uhr<br>
+            <b>Beginn:</b> 21 Uhr
+        </div>
+    </div>
+
+    <span id="oldConcertsHeading">VERGANGENE KONZERTE:</span>
+    <div class="gig">
+        <div class="gigLeft">
+            <h2>Sportlerfasching Spfr. Haßmersheim</h2>
+            <br>
+            <span class="place">Turn- und Festhalle, Teststraße 7, 74855 Haßmersheim</span>
+        </div>
+        <div class="gigRight">
+            <span class="dateSpan">01.02.2024</span><br>
+            <a href="Deine Mama" class="gigLink">Bilder</a><br>
+            <a href="Deine Mama" class="gigLink">Videos</a>
+        </div>
+    </div>
+    <div class="gig">
+        <div class="gigLeft">
+            <h2>Sportlerfasching Spfr. Haßmersheim</h2>
+            <br>
+            <span class="place">Turn- und Festhalle, Teststraße 7, 74855 Haßmersheim</span>
+        </div>
+        <div class="gigRight">
+            <span class="dateSpan">01.02.2024</span><br>
+            <a href="Deine Mama" class="gigLink">Bilder</a><br>
+            <a href="Deine Mama" class="gigLink">Videos</a>
+        </div>
+    </div>
 </body>
+
 </html>
--- start.php
+++ start.php
@@ -1,66 +1,68 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/slideshow.css">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Herzlich Willkommen bei <br><b class="random">RANDOM X</b>!</h1>
 
-	
-	<!-- Slideshow container -->
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="css/slideshow.css">
+  <title>Document</title>
+</head>
+
+<body>
+  <h1>Herzlich Willkommen bei <br><b class="random">RANDOM X</b>!</h1>
+
+
+  <!-- Slideshow container -->
   <div class="slideshow-container">
 
-<?php
+    <?php
 
-$files = glob('img/slideshow/*.{jpg,png,gif}', GLOB_BRACE);
-$filecount = count($files);
-$currentnr = 1;
-foreach($files as $file) {
-  //do your work here
-  
-  
-  echo '<div class="mySlides fade">';
-  echo '  <div class="numbertext">' . $currentnr . ' / ' . $filecount . '</div>';
-  echo '  <img src="' . $file . '" class="slideshowPic">';
-  echo ' </div>';
-  $currentnr++;
-}
-?>
+    $files = glob('img/slideshow/*.{jpg,png,gif}', GLOB_BRACE);
+    $filecount = count($files);
+    $currentnr = 1;
+    foreach ($files as $file) {
+      //do your work here
+    
 
-  <!-- Next and previous buttons -->
-  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-  <a class="next" onclick="plusSlides(1)">&#10095;</a>
-</div>
-<br>
+      echo '<div class="mySlides fade">';
+      echo '  <div class="numbertext">' . $currentnr . ' / ' . $filecount . '</div>';
+      echo '  <img src="' . $file . '" class="slideshowPic">';
+      echo ' </div>';
+      $currentnr++;
+    }
+    ?>
 
-<!-- The dots/circles -->
-<div style="text-align:center">
-<?php
+    <!-- Next and previous buttons -->
+    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+  </div>
+  <br>
 
-$files = glob('img/slideshow/*.{jpg,png,gif}', GLOB_BRACE);
-$currentnr = 1;
-foreach($files as $file) {
-  //do your work here
-  
-  
-  echo '<span class="dot" onclick="currentSlide(' . $currentnr . ')"></span>';
-  $currentnr++;
-}
-?>
-</div>
-    <br>
+  <!-- The dots/circles -->
+  <div style="text-align:center">
+    <?php
 
-    <p>
-      Hi, wir sind <b>RANDOM X</b>, eine Cover-Band aus Haßmersheim! <br>
-       Wir covern alles von Pop bis Rock, von <b>(A)</b>BBA bis <b>(Z)</b>ombie. Schaut euch gerne um, über einen Besuch auf einem unserer nächsten Konzerte würden wir uns sehr freuen!  
-    </p>
+    $files = glob('img/slideshow/*.{jpg,png,gif}', GLOB_BRACE);
+    $currentnr = 1;
+    foreach ($files as $file) {
+      //do your work here
+    
 
-<br><br><br><br><br><br>
+      echo '<span class="dot" onclick="currentSlide(' . $currentnr . ')"></span>';
+      $currentnr++;
+    }
+    ?>
+  </div>
+  <br>
 
-<script src="js/slideshow.js"></script>
+  <p>
+    Hi, wir sind <b>RANDOM X</b>, eine Cover-Band aus Haßmersheim! <br>
+    Wir covern alles von Pop bis Rock, von <b>(A)</b>BBA bis <b>(Z)</b>ombie. Schaut euch gerne um, über einen Besuch
+    auf einem unserer nächsten Konzerte würden wir uns sehr freuen!
+  </p>
+
+  <script src="js/slideshow.js"></script>
 </body>
+
 </html>
